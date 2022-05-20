7e0ff28926ec7d94ed82a2dd8cff458da35dd03b
mstanley.me@gmail.com <mstanley.me@gmail.com>
2022-05-20 19:21:36 +0100

    Added features

--- index.js
+++ index.js
@@ -164,27 +164,52 @@ Projects.forEach((project) => {
   });
 });
 
-document.addEventListener('click', (e) => {
-  const slideUp = document.querySelector('.arrow__Down');
-  const slideDown = document.querySelector('.arrow__Right__00');
-  const Slider = document.querySelector('.Slider');
+function mediaX() {
+  const mediaDesk = window.matchMedia('(min-width: 992px)');
+  const mediaMob = window.matchMedia('(max-width: 991px)');
   const languagesItems = document.getElementById('languagesItems');
-  if (e.target === slideUp) {
-    languagesItems.style.display = 'none';
-    slideUp.style.display = 'none';
-    slideDown.style.display = 'block';
-    Slider.style.marginTop = '10px';
-    Slider.style.borderBottom = '1px solid #d1d1d1';
-    Slider.style.marginBottom = '10px';
-    languagesItems.style.transition = 'all 0.2s ease-in-out';
-  } else if (e.target === slideDown) {
-    languagesItems.style.display = 'block';
-    languagesItems.style.transition = 'all 0.2s ease-in-out';
-    slideDown.style.display = 'none';
-    slideUp.style.display = 'flex';
-    Slider.style.borderBottom = 'none';
+  if (mediaDesk.matches) {
+    document.addEventListener('click', (e) => {
+      const slideUp = document.querySelector('.arrow__Down');
+      const slideDown = document.querySelector('.arrow__Right__00');
+      const Slider = document.querySelector('.Slider');
+      if (e.target === slideUp) {
+        languagesItems.style.display = 'none';
+        slideUp.style.display = 'none';
+        slideDown.style.display = 'block';
+        Slider.style.marginTop = '10px';
+        Slider.style.borderBottom = '1px solid #d1d1d1';
+        Slider.style.marginBottom = '10px';
+        languagesItems.style.transition = 'all 0.2s ease-in-out';
+      } else if (e.target === slideDown) {
+        languagesItems.style.display = 'flex';
+        languagesItems.style.transition = 'all 0.2s ease-in-out';
+        slideDown.style.display = 'none';
+        slideUp.style.display = 'flex';
+        Slider.style.borderBottom = 'none';
+      }
+    });
+  } else if (mediaMob.matches) {
+    document.addEventListener('click', (e) => {
+      const slideUp = document.querySelector('.arrow__Down');
+      const slideDown = document.querySelector('.arrow__Right__00');
+      const Slider = document.querySelector('.Slider');
+      const languagesItems = document.getElementById('languagesItems');
+      if (e.target === slideUp) {
+        languagesItems.style.display = 'none';
+        slideUp.style.display = 'none';
+        slideDown.style.display = 'block';
+        Slider.style.borderBottom = '1px solid #d1d1d1';
+      } else if (e.target === slideDown) {
+        languagesItems.style.display = 'block';
+        slideDown.style.display = 'none';
+        slideUp.style.display = 'flex';
+        Slider.style.borderBottom = 'none';
+      }
+    });
   }
-});
+}
+mediaX();
 
 const formValidation = document.querySelector('#formContainer');
 const emailInputValidation = document.querySelector('#email');
